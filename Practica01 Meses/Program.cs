71b653082c889d37cee0b92ef243db8b1c232fa7
manuel quiroga <manuelquirogaa@hotmail.com>
2020-04-25 19:34:40 -0700

    actividad 1 meses

---
+++ Practica01 Meses/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica01_Meses
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] mes = new string[] { "ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO", "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE" };
+            int i = 0;
+            int m;
+            do
+            {
+                Console.WriteLine("Ingresa Un Numero: ");
+                m = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("El Nombre Del Mes Es:" + mes[m]);
+                i = i + 1;
+            } while (i < mes.Length);
+
+        }
+    }
+}
